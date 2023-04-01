b0b495de2176dd1984e09c760487ee31f458a520
Pavitra Golchha <hello@pavi2410.me>
2023-04-01 19:37:57 +0530

    Fix everything except multiselect

--- example_app_with_permalinks.py
+++ example_app_with_permalinks.py
@@ -1,44 +1,49 @@
 import streamlit as st
-from datetime import date, datetime, time
 
 import streamlit_permalink as stp
 
-# is_checked = stp.checkbox('checkbox', url_key='checkbox')
-# is_checked_default = stp.checkbox('checkbox default', value=True, url_key='checkbox_default')
-# radio = stp.radio('radio', ['Option A', 'Option B', 'Option C'], url_key='radio')
-# selectbox = stp.selectbox('selectbox', ['Option A', 'Option B', 'Option C'], url_key='selectbox')
-# multiselect = stp.multiselect('multiselect', ['Zażółć gęślą jaźń', 'A', 'Grzegżółka', 'B', 'Brzęczyszczykiewicz', '/%^&+$#@!~`\"\'[]{}'], default=['A', 'B'], url_key='multiselect')
-# slider = stp.slider('slider', min_value=1, max_value=100, value=[42, 67], url_key='slider')
-# select_slider = stp.select_slider('select_slider', list(range(10)), value='5', url_key='select_slider')
-# text_input = stp.text_input('text_input', value='xxx', url_key='text_input')
-# number_input = stp.number_input('number_input', min_value=1, max_value=100, value=42, url_key='number_input')
-# text_area = stp.text_area('text_area', url_key='text_area')
-# date_input = stp.date_input('date_input', url_key='date_input')
-# time_input = stp.time_input('time_input', url_key='time_input')
-# color_picker = stp.color_picker('color_picker', value='#00EEFF', url_key='color_picker')
+is_checked = stp.checkbox('checkbox', url_key='checkbox')
+is_checked_default = stp.checkbox('checkbox default', value=True, url_key='checkbox_default')
+radio = stp.radio('radio', ['Option A', 'Option B', 'Option C'], url_key='radio')
+selectbox = stp.selectbox('selectbox', ['Option A', 'Option B', 'Option C'], url_key='selectbox')
+# multiselect = stp.multiselect('multiselect', ['Zażółć gęślą jaźń', 'A', 'Grzegżółka', 'B', 'Brzęczyszczykiewicz',
+#                                               '/%^&+$#@!~`\"\'[]{}'], default=['A', 'B'], url_key='multiselect')
+slider = stp.slider('slider', min_value=1, max_value=100, value=[42, 67], url_key='slider')
+select_slider = stp.select_slider('select_slider', ['apple', 'banana', 'mango', 'orange'], value='banana',
+                                  url_key='select_slider')
+text_input = stp.text_input('text_input', value='xxx', url_key='text_input')
+number_input = stp.number_input('number_input', min_value=1, max_value=100, value=42, url_key='number_input')
+text_area = stp.text_area('text_area', url_key='text_area')
+date_input = stp.date_input('date_input', url_key='date_input')
+time_input = stp.time_input('time_input', url_key='time_input')
+color_picker = stp.color_picker('color_picker', value='#00EEFF', url_key='color_picker')
+
+with st.sidebar:
+    st.write(dict(
+        is_checked=is_checked,
+        is_checked_default=is_checked_default,
+        radio=radio,
+        selectbox=selectbox,
+        # multiselect=multiselect,
+        slider=slider,
+        select_slider=select_slider,
+        text_input=text_input,
+        number_input=number_input,
+        text_area=text_area,
+        date_input=date_input,
+        time_input=time_input,
+        color_picker=color_picker,
+    ))
 
 with stp.form('form'):
-    selectbox = stp.selectbox('selectbox', ['Option A', 'Option B', 'Option C'], url_key='selectbox')
-    text_input = stp.text_input('text_input', value='xxx', url_key='text_input')
-    time_input = stp.time_input('time_input', url_key='time_input')
+    selectbox = stp.selectbox('selectbox', ['Option A', 'Option B', 'Option C'], url_key='form_selectbox')
+    text_input = stp.text_input('text_input', value='xxx', url_key='form_text_input')
+    time_input = stp.time_input('time_input', url_key='form_time_input')
     stp.form_submit_button('Submit')
 
-loc = locals().copy()
-irrelevant = [
-    "__name__",
-    "__doc__",
-    "__package__",
-    "__loader__",
-    "__spec__",
-    "__file__",
-    "__builtins__",
-    "st",
-    "__streamlit__",
-    "stp",
-    "datetime",
-    "date",
-    "time",
-]
-for i in irrelevant:
-    del loc[i]
-st.write(loc)
+with st.sidebar:
+    st.write(dict(
+        selectbox=selectbox,
+        text_input=text_input,
+        time_input=time_input,
+    ))
--- streamlit_permalink.py
+++ streamlit_permalink.py
@@ -59,7 +59,7 @@ class UrlAwareWidget:
 
         def on_change(*args, **kwargs):
             qp = st.experimental_get_query_params()
-            qp[url_key] = to_url_value(getattr(st.session_state, key))
+            qp[url_key] = to_url_value(st.session_state[key])
             st.experimental_set_query_params(**qp)
             user_supplied_change_handler(*args, **kwargs)
 
@@ -75,8 +75,7 @@ class UrlAwareWidget:
             value = None
 
         handler = getattr(self, f'handle_{self.base_widget.__name__}')
-        result = handler(value, *args, **kwargs)
-        return result
+        return handler(value, *args, **kwargs)
 
     def call_inside_form(self, form, *args, **kwargs):
         url_key = kwargs.pop('url_key')
@@ -87,55 +86,56 @@ class UrlAwareWidget:
         url = st.experimental_get_query_params()
         url_value = url.get(url_key, None)
         handler = getattr(self, f'handle_{self.base_widget.__name__}')
-        _, result = handler(url_value, *args, **kwargs)
-        return result
+        return handler(url_value, *args, **kwargs)
 
     def handle_checkbox(self, url_value, label, value=False, *args, **kwargs):
         url_value = url_value and url_value[0]
         url_value = {'True': True, 'False': False}.get(url_value, url_value)
         if url_value is not None:
             value = url_value
-        result = self.base_widget(label, value, *args, **kwargs)
-        return result
+        return self.base_widget(label, value, *args, **kwargs)
 
     def handle_radio(self, url_value, *args, **kwargs):
         return self.handle_selectbox(url_value, *args, **kwargs)
 
     def handle_selectbox(self, url_value, label, options, index=0, *args, **kwargs):
-        url_value = url_value and url_value[0]
         options = list(map(str, options))
-        if url_value is not None:
+        if url_value is not None and len(url_value) == 1:
             try:
-                index = options.index(url_value)
+                index = options.index(url_value[0])
             except ValueError:
                 pass
-        result = self.base_widget(label, options, index, *args, **kwargs)
-        return result
+        return self.base_widget(label, options, index, *args, **kwargs)
 
     def handle_multiselect(self, url_value, label, options, default=None, *args, **kwargs):
         options = list(map(str, options))
         if url_value is not None:
             default = url_value
-        result = self.base_widget(label, options, default, *args, **kwargs)
-        return result
+        return self.base_widget(label, options, default, *args, **kwargs)
 
     def handle_slider(self, url_value, label, min_value=None, max_value=None, value=None, *args, **kwargs):
-        if value is not None and not isinstance(value, list):
-            slider_type = type(value)
-        if value is not None and isinstance(value, list):
-            slider_type = type(value[0])
+        slider_type = float
+        if value is not None:
+            if isinstance(value, list):
+                slider_type = type(value[0])
+            else:
+                slider_type = type(value)
         elif min_value is not None:
             slider_type = type(min_value)
         elif max_value is not None:
             slider_type = type(max_value)
-        assert slider_type in (int, float), "unsupported slider type"
+        assert slider_type in (int, float), f"unsupported slider type: {slider_type}"
+
         if url_value is not None:
             if len(url_value) == 1:
-                value = slider_type(float(url_value[0]))
+                if isinstance(url_value[0], tuple):
+                    value = [slider_type(v) for v in url_value[0]]
+                else:
+                    value = slider_type(url_value[0])
             else:
-                value = [slider_type(float(i)) for i in url_value]
-        result = self.base_widget(label, min_value, max_value, value, *args, **kwargs)
-        return result
+                value = [slider_type(v) for v in url_value]
+
+        return self.base_widget(label, min_value, max_value, value, *args, **kwargs)
 
     def handle_select_slider(self, url_value, label, options, value=None, *args, **kwargs):
         options = list(map(str, options))
@@ -144,14 +144,12 @@ class UrlAwareWidget:
                 value = url_value[0]
             else:
                 value = url_value
-        result = self.base_widget(label, options, value, *args, **kwargs)
-        return result
+        return self.base_widget(label, options, value, *args, **kwargs)
 
     def handle_text_input(self, url_value, label, value="", *args, **kwargs):
         if url_value is not None:
             value = url_value[0]
-        result = self.base_widget(label, value, *args, **kwargs)
-        return result
+        return self.base_widget(label, value, *args, **kwargs)
 
     def handle_number_input(self, url_value, label, min_value=None, max_value=None, value=None, *args, **kwargs):
         input_type = float
@@ -161,14 +159,14 @@ class UrlAwareWidget:
             input_type = type(min_value)
         elif max_value is not None:
             input_type = type(max_value)
-        assert input_type in (int, float), "unsupported number_input type"
+        assert input_type in (int, float), f"unsupported number_input type: {input_type}"
+
         if url_value is not None:
             value = input_type(float(url_value[0]))
+
         if value is None:
-            result = self.base_widget(label, min_value, max_value, *args, **kwargs)
-        else:
-            result = self.base_widget(label, min_value, max_value, value, *args, **kwargs)
-        return result
+            return self.base_widget(label, min_value, max_value, *args, **kwargs)
+        return self.base_widget(label, min_value, max_value, value, *args, **kwargs)
 
     def handle_text_area(self, url_value, *args, **kwargs):
         return self.handle_text_input(url_value, *args, **kwargs)
@@ -177,24 +175,28 @@ class UrlAwareWidget:
         parse_date = lambda s: datetime.strptime(s, '%Y-%m-%d').date()
         if url_value is not None:
             if len(url_value) == 1:
-                value = parse_date(url_value[0])
+                if isinstance(url_value[0], date):
+                    value = url_value[0]
+                else:
+                    value = parse_date(url_value[0])
             else:
-                value = list(map(parse_date, url_value))
-        result = self.base_widget(label, value, *args, **kwargs)
-        return result
+                value = [parse_date(v) for v in url_value]
+        return self.base_widget(label, value, *args, **kwargs)
 
     def handle_time_input(self, url_value, label, value=None, *args, **kwargs):
         parse_time = lambda s: datetime.strptime(s, '%H:%M').time()
         if url_value is not None:
-            value = parse_time(url_value[0])
-        result = self.base_widget(label, value, *args, **kwargs)
-        return result
+            if len(url_value) == 1:
+                if isinstance(url_value[0], time):
+                    value = url_value[0]
+                else:
+                    value = parse_time(url_value[0])
+        return self.base_widget(label, value, *args, **kwargs)
 
     def handle_color_picker(self, url_value, label, value=None, *args, **kwargs):
         if url_value is not None:
             value = url_value[0]
-        result = self.base_widget(label, value, *args, **kwargs)
-        return result
+        return self.base_widget(label, value, *args, **kwargs)
 
 
 class UrlAwareFormSubmitButton:
